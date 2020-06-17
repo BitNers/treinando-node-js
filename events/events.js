0dc285d8b2eb50d0c9b5d518232f1815cf3297f0
BitNers <wesleyalvessilva@outlook.com.br>
2020-06-17 17:44:52 -0300

    API Eventos.
    Aprendendo sobre a API Nativa de Eventos e entendendo sobre Subscribers e Publishers.

--- events/events.js
+++ events/events.js
@@ -1,10 +1,19 @@
-const EventEmitter = require('events')
+const {EventEmitter} = require('events')
 
 class Evento extends EventEmitter {}
 const meuEvento = new Evento();
 
+// Subscriber - Assinante
 meuEvento.on('Segurança', (x,y)=>{
     console.log(`Executando o evento: ${x} e ${y}.`);
 });
 
-meuEvento.emit('Segurança', 'userAdmin','Salario');
+
+// Publisher - Emissor
+meuEvento.emit('Segurança', 'userAdmin','Salario'); 
+
+meuEvento.on('Encerrar', (data)=>{
+    console.log('Assinante: '+data)
+})
+
+meuEvento.emit('Encerrar','Encerrando a execução.')
