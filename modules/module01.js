a9fc67bd198253d8fd0c183f70ac858912797bc5
BitNers <wesleyalvessilva@outlook.com.br>
2020-06-23 10:34:32 -0300

    Finalizando o Core e API Nativas do Node.
    Finalizando o Core e Natives API do NodeJs. Dando início a API Express para desenvolvimento Web.

---
+++ modules/module01.js
@@ -0,0 +1,12 @@
+// console.log("Executando módulo 01")
+
+const oculta = ()=>{
+    console.log("Função oculta.")
+}
+
+executa = () =>{
+    console.log("Função Executa")
+}
+
+welcome = `Bem-vindo ao módulo 01 JS.`
+module.exports = {executa, welcome}
---
+++ modules/module02.js
@@ -0,0 +1,7 @@
+const { executa, welcome } = require("./module01")
+
+// const module01 = require("./module01")
+const {executa} = require("./module01")
+
+oculta()
+executa()
